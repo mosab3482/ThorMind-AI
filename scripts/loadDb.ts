33e3f2d59f65ea2f7bda5345b0dc605b02d64540
mosab3482 <mow3482@gmail.com>
2026-04-27 03:13:41 +0200

    Initial commit

---
+++ scripts/loadDb.ts
@@ -0,0 +1,16 @@
+import { DataAPIClient } from "@datastax/astra-db-ts";
+import { PuppeteerWebBaseLoader } from "@langchain/community/document_loaders/web/puppeteer";
+import OpenAI from "openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import "dotenv/config";
+const {
+  ASTRA_DB_NAMESPACE,
+  ASTRA_DB_COLLECTION,
+  ASTA_DB_API_ENDPOINT,
+  ASTRA_DB_APPLICATION_TOKEN,
+  OPENAI_API_KEY,
+} = process.env;
+
+const openai = new OpenAI({ apiKey: OPENAI_API_KEY });
+
+const throData = [];
